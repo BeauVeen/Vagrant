9f2893dfe1aee15f5a3c799406559efb4c00a0a2
Beau van der Veen <beauvanderveen18@gmail.com>
2025-03-05 17:45:26 +0100

    Delete mysql_setup.sh

--- mysql_setup.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-mysql -Bse "CREATE DATABASE wordpress;CREATE USER 'wpadmin'@'localhost' IDENTIFIED BY 'P@ssword1';GRANT ALL ON wordpress.* TO 'wpadmin'@'localhost';FLUSH PRIVILEGES;"
